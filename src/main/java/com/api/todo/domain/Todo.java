ac74c827ebcc66fbb381b4f020cdb94a904772a3
Nailton2002 <jnmp25jnmp@gmail.com>
2023-05-22 20:11:11 -0300

    Ajuste na classe Todo pasando a classe TodoSavarDto como parametro no metodo

--- src/main/java/com/api/todo/domain/Todo.java
+++ src/main/java/com/api/todo/domain/Todo.java
@@ -1,5 +1,6 @@
 package com.api.todo.domain;
 
+import com.api.todo.dto.TodoSalvarDto;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
@@ -20,5 +21,12 @@ public class Todo implements Serializable {
      private String descricao;
      @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd't'HH:mm:ss'Z'", timezone = "GMT")
      private LocalDateTime dataTarefaFinalizada = LocalDateTime.now();
-     private Boolean tarefaFinalizada = false;
+     private Boolean tarefaFinalizada;
+
+     public Todo (TodoSalvarDto dados){
+          this.tarefaFinalizada = false;
+          this.titulo = dados.titulo();
+          this.descricao = dados.descricao();
+          this.dataTarefaFinalizada = dados.dataParaFinalizar();
+     }
 }

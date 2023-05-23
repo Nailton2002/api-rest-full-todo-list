f9bd307197d22c8a1d1b92a59575172073a6a64a
Nailton2002 <jnmp25jnmp@gmail.com>
2023-05-22 22:34:15 -0300

    Ajustes na classe  TodoSalvarDto

--- src/main/java/com/api/todo/dto/TodoSalvarDto.java
+++ src/main/java/com/api/todo/dto/TodoSalvarDto.java
@@ -9,5 +9,6 @@ public record TodoSalvarDto(
         String titulo,
         String descricao,
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd't'HH:mm:ss'Z'", timezone = "GMT")
-        LocalDateTime dataParaFinalizar
+        LocalDateTime dataParaFinalizar,
+        Boolean tarefaFinalizada
 ) { }

533ab460d090576a8ac70051e387547c22e7b36b
Nailton2002 <jnmp25jnmp@gmail.com>
2023-05-23 09:29:38 -0300

    Criando a query de tarefas a ser feitas.

--- src/main/java/com/api/todo/repository/TodoRepository.java
+++ src/main/java/com/api/todo/repository/TodoRepository.java
@@ -1,7 +1,16 @@
 package com.api.todo.repository;
 
 import com.api.todo.domain.Todo;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
+
+    Page<Todo> findAllByTarefaFinalizadaFalse(Pageable paginacao);
+
+
 }

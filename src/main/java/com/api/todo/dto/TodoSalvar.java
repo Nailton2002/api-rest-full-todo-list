fdf10478390e4bbe0178d321f39b8a4792afbaec
Nailton2002 <jnmp25jnmp@gmail.com>
2023-05-23 13:54:58 -0300

    Ajuste no parametro da classe TodoSalvarDto.

--- src/main/java/com/api/todo/dto/TodoSalvar.java
+++ src/main/java/com/api/todo/dto/TodoSalvar.java
@@ -4,9 +4,9 @@ import com.fasterxml.jackson.annotation.JsonFormat;
 
 import java.time.LocalDateTime;
 
-public record TodoSalvarDto(
+public record TodoSalvar(
         String titulo,
         String descricao,
         @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd't'HH:mm:ss'Z'", timezone = "GMT")
-        LocalDateTime dataParaFinalizar
+        LocalDateTime dataTarefaFinalizada
 ) { }

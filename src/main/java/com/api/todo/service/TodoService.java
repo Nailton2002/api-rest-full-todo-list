8d39a9569582f0abbbdd620e1cacb4e55c0ea684
Nailton2002 <jnmp25jnmp@gmail.com>
2023-05-22 22:33:49 -0300

    Adcionando o metodo findAll no service

--- src/main/java/com/api/todo/service/TodoService.java
+++ src/main/java/com/api/todo/service/TodoService.java
@@ -1,10 +1,14 @@
 package com.api.todo.service;
 
 import com.api.todo.domain.Todo;
+import com.api.todo.dto.TodoListar;
 import com.api.todo.dto.TodoSalvarDto;
 import com.api.todo.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TodoService {
@@ -13,7 +17,12 @@ public class TodoService {
     private TodoRepository repository;
 
     public Todo create(TodoSalvarDto dados){
-
         return repository.save(new Todo(dados));
+    }
+
+    public List<TodoListar> findAll(){
+        List<Todo> list = repository.findAll();
+        List<TodoListar> dto = list.stream().map(t -> new TodoListar(t)).collect(Collectors.toList());
+        return dto;
     }
 }

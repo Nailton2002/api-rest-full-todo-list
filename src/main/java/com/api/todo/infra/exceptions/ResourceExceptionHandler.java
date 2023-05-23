98f77fc2d747ab1644ca5c52a299fefb0cb94303
Nailton2002 <jnmp25jnmp@gmail.com>
2023-05-23 12:44:14 -0300

    Criando a classes ResourceExceptionHandler e os metodos objectNotFound.

---
+++ src/main/java/com/api/todo/infra/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,29 @@
+package com.api.todo.infra.exceptions;
+
+import com.api.todo.infra.validation.ObjectNotFoundException;
+import com.api.todo.infra.validation.ResourceNotFoundException;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.time.Instant;
+@ControllerAdvice
+public class ResourceExceptionHandler{
+	@ExceptionHandler(ObjectNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ObjectNotFoundException e, HttpServletRequest request) {
+		String error = "Object not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError erro = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(erro);
+	}
+
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> objectNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		String error = "Object not found";
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError erro = new StandardError(Instant.now(), status.value(), error, e.getMessage(), request.getRequestURI());
+		return ResponseEntity.status(status).body(erro);
+	}
+}

20b1f1df5cf7c85232949fb0c51ef306e238097b
Nailton2002 <jnmp25jnmp@gmail.com>
2023-05-23 20:38:28 -0300

    Criando as query findAllOpen e findAllClose no repository.

--- src/main/java/com/api/todo/repository/TodoRepository.java
+++ src/main/java/com/api/todo/repository/TodoRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface TodoRepository extends JpaRepository<Todo, Long> {
@@ -13,5 +14,12 @@ public interface TodoRepository extends JpaRepository<Todo, Long> {
     Page<Todo> findAllByTarefaFinalizadaFalse(Pageable paginacao);
     Page<Todo> findAllByTarefaFinalizadaTrue(Pageable paginacao);
 
+    //TAREFA INICIADA = ABERTA
+    @Query("SELECT obj FROM Todo obj WHERE obj.tarefaFinalizada = false ORDER BY obj.dataTarefaFinalizada")
+    List<Todo> findAllOpen();
+
+    //TAREFA FINALIZADA = FECHADA
+    @Query("SELECT obj FROM Todo obj WHERE obj.tarefaFinalizada = true ORDER BY obj.dataTarefaFinalizada")
+    List<Todo> findAllClose();
 
 }

01591d6d43361c83e5f8f52a6dc8d380239dd41f
Nailton2002 <jnmp25jnmp@gmail.com>
2023-05-24 17:39:10 -0300

    Devolvendo um dto no metodo de efetuarLogin.

--- src/main/java/com/api/todo/controller/AutenticacaoController.java
+++ src/main/java/com/api/todo/controller/AutenticacaoController.java
@@ -1,6 +1,9 @@
 package com.api.todo.controller;
 
 import com.api.todo.domain.usuario.dto.DadosAutenticacao;
+import com.api.todo.domain.usuario.entity.Usuario;
+import com.api.todo.infra.security.DadosTokenJWT;
+import com.api.todo.infra.security.TokenService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -18,10 +21,14 @@ public class AutenticacaoController {
     @Autowired
     private AuthenticationManager manager;
 
+    @Autowired
+    private TokenService tokenService;
+
     @PostMapping
     public ResponseEntity efetuarLogin(@RequestBody @Valid DadosAutenticacao dados){
         var token =new UsernamePasswordAuthenticationToken(dados.login(), dados.senha());
         var authentication = manager.authenticate(token);
-        return ResponseEntity.ok().build();
+        var tokenJWT = tokenService.gerarToken((Usuario) authentication.getPrincipal());
+        return ResponseEntity.ok(new DadosTokenJWT(tokenJWT));
     }
 }
